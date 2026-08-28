e1eb516416e107651cb33c8e22f2851098eab3fc
Ali Hassan <itx.alihassanch.com>
2026-08-29 00:44:31 +0500

    BackEnd setUP

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import app from "./src/app.js";
 import { configDotenv } from "dotenv";
 
-const port = process.env.PORT 
+const port = process.env.PORT
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost${port} 😎`)
-})
+async function startServer() {
+    app.listen(port, () => {
+        console.log(`Server is running on http://localhost${port} 😎`)
+    })
+}
+
+startServer()
