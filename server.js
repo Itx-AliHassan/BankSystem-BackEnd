a216d85b2405d47fcbe1488683317d7ce634411a
Ali Hassan <itx.alihassanch@gmail.com>
2026-09-02 18:40:00 +0500

    auth api setup

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ require('dotenv').config()
 const app = require('./src/app')
 
 const port = process.env.PORT
-const connectDB = require('./src/DB/db')
+const connectDB = require('./src/config/db')
 
 async function startServer() {
     await connectDB()
 
     app.listen(port, () => {
-        console.log(`Server is running on http://localhost${port} 😎`)
+        console.log(`Server is running on http://localhost:${port} 😎`)
     })
 }
 
---
+++ src/models/user.model.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs')
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+    },
+    email: {
+        type: String,
+        required: [true, 'Email is required to create an account'],
+        unique: [true, 'Email already exists'],
+        trim: true,
+        lowercase: true,
+        match: [/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/, 'Invalid email format'],
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: [6, 'Password Should be more then 6 character'],
+        select: false
+    },
+}, { timestamps: true });
+
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next()
+    const hash = await bcrypt.hash(this.password, process.env.SAULT)
+    this.password = hash
+    return next()
+})
+
+userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+
+
+const userModel = mongoose.model('User', userSchema);
+
+module.exports = userModel;
